ed135a9043f97088df888a08ac560ebd7cde897a
Maximeswd <maximedassen@yahoo.com>
2023-04-06 17:49:48 +0200

    EDA

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,16 +1,36 @@
 from dateutil.parser import parse 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from datetime import datetime
 import seaborn as sns
 import numpy as np
 import pandas as pd
-plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
+
+# https://www.machinelearningplus.com/time-series/time-series-analysis-python/#google_vignette
+# https://www.python-graph-gallery.com/basic-time-series-with-matplotlib?utm_content=cmp-true
+# https://unidata.github.io/python-training/workshop/Time_Series/basic-time-series-plotting/
+
 
 # Import as Dataframe
-df = pd.read_csv('/Users/gast/Desktop/VS code/Artificial Intelligence Projects/Data Mining Techniques/Data/dataset_mood_smartphone.csv')
-df.head()
-list(df.columns)
-df = df.iloc[: , 1:]
+data = pd.read_csv('./Data/dataset_mood_smartphone.csv')
+data.head()
+data['time'] = pd.to_datetime(data['time'])
+
+
+# Inspect the dataset
+print(f"Number of records: {len(data)}")
+print(f"Number of attributes: {len(data.columns)}")
+print(f"Attribute types:\n{data.dtypes}")
+print(f"Ranges of values:\n{data['value'].describe()}")
+
+# Check for missing values
+print(f"Number of missing values:\n{data.isnull().sum()}") # 202 missing values in the 'value' column
+
+# Generate descriptive statistics for the dataset
+print(data.describe())
+
+# Counts of values recorded per month
+pd.to_datetime(data["time"]).dt.to_period('M').value_counts().sort_index()
 
 
 # Draw Plot
@@ -20,23 +40,14 @@ def plot_df(df, x, y, title="", xlabel='Date', ylabel='Value', dpi=100):
     plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
     plt.show()
 
-plot_df(df, x = df.index, y = df.value, title='Values over time.')    
-
-# Counts of values recorded per month
-pd.to_datetime(df["time"]).dt.to_period('M').value_counts().sort_index()
+plot_df(data, x = data.index, y = data.value, title='Values over time.')    
 
 
-plt.rc('font', size=12)
-fig, ax = plt.subplots(figsize=(10, 6))
-
-# Specify how our lines should look
-ax.plot(df.time, df.values, color='tab:orange', label='Windspeed')
-
-# Same as above
-ax.set_xlabel('j')
-ax.set_ylabel('joe')
-ax.set_title('..')
-ax.grid(True)
-ax.legend(loc='upper left')
+# Create line plot of mood variable over time for each participant
+plt.figure(figsize=(12, 6))
+sns.lineplot(x='time', y='value', data=data[data['variable'] == 'mood'], hue='id')
+plt.title('Mood over time by participant')
+plt.xlabel('Time')
+plt.ylabel('Mood')
 plt.show()
 
