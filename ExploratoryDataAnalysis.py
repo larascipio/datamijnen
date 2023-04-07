6cc56352ae4a077bdce7f119db854dec95edcdc4
Maximeswd <maximedassen@yahoo.com>
2023-04-07 11:11:22 +0200

    changes

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -6,14 +6,23 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
+from Preprocessing.preprocessing import print_z_score_limits
+from Preprocessing.preprocessing import remove_outliers
+
+
 # https://www.machinelearningplus.com/time-series/time-series-analysis-python/#google_vignette
 # https://www.python-graph-gallery.com/basic-time-series-with-matplotlib?utm_content=cmp-true
 # https://unidata.github.io/python-training/workshop/Time_Series/basic-time-series-plotting/
 
 
 # Import as Dataframe
-data = pd.read_csv('./Data/dataset_mood_smartphone.csv')
-data.head()
+df = pd.read_csv('./Data/dataset_mood_smartphone.csv')
+df.head()
+
+# Drop unnecessary columns
+data =  df.drop(['Unnamed: 0'], axis=1)
+    
+# Make sure the 'time' column is of type datetime
 data['time'] = pd.to_datetime(data['time'])
 
 
@@ -50,4 +59,35 @@ plt.title('Mood over time by participant')
 plt.xlabel('Time')
 plt.ylabel('Mood')
 plt.show()
+
+
+# Data not normally distributed, a lot of extreme values
+# Show histograms - all variables except for the identifier variables
+plt.hist(np.array(data['value'].tolist()).astype('float'), density=False, bins=30)  # density=False would make counts
+plt.ylabel('Probability')
+plt.xlabel('Data')
+plt.show()
+
+
+
+
+# Outlier removal based on Z-score solely (NOT GOOD BECAUSE NOT NORMALLY DISTRIBUTED)
+# Print the upper and lower limits
+print_z_score_limits(data, "value")
+'Variable Name: value | Upper limit: 861.84 | Lower limit: -780.51'
+
+# Filter outliers
+sample_z = data[(data['value'] >= 861.84) | (data['value'] <= -780.51)]
+print(sample_z.shape) # (1694, 4) --> 1694 outliers according to Z-score
+
+
+# Outlier removal based on IQR
+c_data = remove_outliers(data)
+
+# Density plot of the cleaned data after outlier removal
+plt.hist(np.array(c_data['value'].tolist()).astype('float'), density=True, bins=30)  # density=False would make counts
+plt.ylabel('Probability')
+plt.xlabel('Data')
+plt.show()
+
 
