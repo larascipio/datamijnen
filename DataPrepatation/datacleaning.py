9cb6a557eeea67920e7e1533a88a5c2914b3e498
Maximeswd <maximedassen@yahoo.com>
2023-04-12 17:25:06 +0200

    latest

--- DataPrepatation/datacleaning.py
+++ DataPrepatation/datacleaning.py
@@ -3,24 +3,92 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 
-# Create a function to report the limits of the Z-Score (Not really used in this project) as it is not a good method 
-# for detecting outliers when the data is not normally distributed.
-def print_z_score_limits(df, column_name):
-    """ Print the upper and lower limits of the Z-score """
+# Import as Dataframe
+df = pd.read_csv('./Data/dataset_mood_smartphone.csv')
+df.head()
+
+# Drop unnecessary columns
+data =  df.drop(['Unnamed: 0'], axis=1)
     
-    # Compute the limits (3 = threshold: default)
-    upper_limit = df[column_name].mean() + 3 * df[column_name].std()
-    lower_limit = df[column_name].mean() - 3 * df[column_name].std()
+# Make sure the 'time' column is of type datetime
+data['time'] = pd.to_datetime(data['time'])
+
+
+# Deleting incorrect values from the dataset 
+
+# Put the above code inside a function 
+def remove_incorrect_values(input_data):
     
-    # Round and return the limits
-    upper_limit = round(upper_limit, 2)
-    lower_limit = round(lower_limit, 2)
-    print_this = "Variable Name: " + column_name + " | Upper limit: " + str(upper_limit) + " | Lower limit: " + str(lower_limit)
-    return(print_this)
+    # Set the valid ranges for each variable
+    valid_ranges = {'mood': (1, 10),
+                    'circumplex.arousal': (-2, 2),
+                    'circumplex.valence': (-2, 2),
+                    'activity': (0, 1),
+                    'screen': (0, 1000000000),
+                    'call': (0, 1),
+                    'sms': (0, 1),
+                    'appCat.builtin': (0, 1000000000),
+                    'appCat.communication': (0, 100000000),
+                    'appCat.entertainment': (0, 1000000000),
+                    'appCat.finance': (0, 1000000000),
+                    'appCat.game': (0, 1000000000),
+                    'appCat.office': (0, 1000000000),
+                    'appCat.other': (0, 1000000000),
+                    'appCat.social': (0, 1000000000),
+                    'appCat.travel': (0, 1000000000),
+                    'appCat.unknown': (0, 1000000000),
+                    'appCat.utilities': (0, 1000000000),
+                    'appCat.weather': (0, 1000000000)}
+    
+    # Filter the dataframe to remove rows with values outside the valid ranges, however, keep the NaN values
+    valid_df = data[data.apply(lambda x: valid_ranges[x.variable][0] <= x.value <= valid_ranges[x.variable][1] if not pd.isnull(x.value) else True, axis=1)]
+    
+    # Create a separate dataframe for the removed rows
+    removed_df = input_data[~input_data.index.isin(valid_df.index)]
+    
+    # Return the valid and removed dataframes
+    return valid_df, removed_df
+
+valid_df, removed_df = remove_incorrect_values(data)
+
+
+# Split the time into date and time
+valid_df['date'] = valid_df['time'].dt.date
+valid_df['time'] = valid_df['time'].dt.time
+
+# Create a new long format dataframe where each row is a unique combination of id and date and the variables are the mean of the values for that day 
+new_df = valid_df.pivot_table(index=['id', 'date'], 
+                          columns='variable', 
+                          values='value', 
+                          aggfunc='mean').reset_index()
+
+# Check the percentage of missing values in the new dataframe for each column
+new_df.isnull().sum() / len(new_df) * 100
+
+# Drop columns that contain more than 50% missing values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Isolation Forest Algorithm
 def run_isolation_forest(model_data: pd.DataFrame, 
+                         column_name: str,
                          contamination = 0.005, 
                          n_estimators= 200, 
                          max_samples = 0.7) -> pd.DataFrame:
@@ -32,14 +100,14 @@ def run_isolation_forest(model_data: pd.DataFrame,
     
     # Drop any rows with NaN values in the 'value' column and store the result in a new dataframe
     df = model_data.copy()
-    df.dropna(subset=['value'], inplace=True)
+    df.dropna(subset=[column_name], inplace=True)
     
     # Fit the model
-    IF.fit(df[['value']])
+    IF.fit(df[[column_name]])
     
     # Datapoints classified -1 are anomalous (outliers): made 1 here for plotting
-    output = pd.Series(IF.predict(df[['value']])).apply(lambda x: 1 if x == -1 else 0)
-    score = IF.decision_function(df[['value']])
+    output = pd.Series(IF.predict(df[[column_name]])).apply(lambda x: 1 if x == -1 else 0)
+    score = IF.decision_function(df[[column_name]])
 
     # Convert the array to a dataframe with a column name 'score'
     score_df = pd.DataFrame({'score': score})
@@ -52,167 +120,133 @@ def run_isolation_forest(model_data: pd.DataFrame,
     merged_df = pd.concat([df, merged_df], axis=1)
     
     # Rename the columns
-    merged_df.columns = ['id', 'time', 'variable', 'value', 'score', 'anomaly']
-
+    merged_df.columns = ['id', 'time', 'activity', 'circumplex.arousal', 'circumplex.valence', 'mood', 'score', 'anomaly']
+    
     return merged_df
 
 
 
 
-# Make a function to impute missing values, based on ARIMA modeling.
-# It can be used to model the time series and then forecast the missing values using the estimated model. 
-# This method works well when the time series exhibits a complex pattern that cannot be captured by a simple linear trend.
+# Number of outliers in activity: 14
+# Number of outliers in circumplex.arousal: 10
+# Number of outliers in circumplex.valence: 8
+# Number of outliers in mood: 27
 
-# Implementing ARIMA, like so:
-from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
 
-# Create the ARIMA model
-def create_arima_model(train, test, p, d, q):
-    """ Create the ARIMA model """
+
+# For the columns 'activity', 'circumplex.arousal', 'circumplex.valence', 'mood' remove the outliers from the 'new' dataframe
+for i in ['activity', 'circumplex.arousal', 'circumplex.valence', 'mood']:
+    # print(f"Number of outliers in {i}: {run_isolation_forest(new, i)[0]['anomaly'].sum()}")
     
-    # Create the model
-    model = ARIMA(train, order=(p, d, q))
-    model_fit = model.fit(disp=0)
+    # Get the final cleaned_df after removing outliers for the column 'i'
+    merged_df = run_isolation_forest(new, i)[0]
     
-    # Make predictions
-    predictions = model_fit.forecast(steps=len(test))[0]
+    # Only select rows where the 'anomaly' column is 0, like so:
+    cleaned_df = merged_df[merged_df['anomaly'] == 0]
+
+    # Drop the 'anomaly' and 'score' columns
+    cleaned_df = cleaned_df.drop(['anomaly', 'score'], axis=1)
     
-    # Compute the error
-    error = mean_squared_error(test, predictions)
-    
-    # Return the predictions and the error
-    return predictions, error
-
-
-
-# Implement Deep learning methods such as recurrent neural networks (RNNs) and Long short-term memory (LSTM) networks 
-# to impute missing values in time series data. These methods work well when the time series exhibits a complex pattern 
-# that cannot be captured by a simple linear trend and when there is a large amount of data available for training.
-
-# Implement the Long short-term memory (LSTM) network, like so:
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-
-# Create the LSTM model
-def create_lstm_model(train, batch_size, epochs, neurons):
-    """ Create the LSTM model """
-    
-    # Reshape the data
-    train = train.reshape(train.shape[0], 1, train.shape[1])
-    
-    # Create the model
-    model = Sequential()
-    model.add(LSTM(neurons, batch_input_shape=(batch_size, train.shape[1], train.shape[2]), stateful=True))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    
-    # Fit the model
-    for i in range(epochs):
-        model.fit(train, train, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
-        model.reset_states()
-    
-    # Return the model
-    return model
-
-
-# Implement the Recurrent neural network (RNN) model, like so:
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import SimpleRNN
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-
-# Create the RNN model
-def create_rnn_model(train, batch_size, epochs, neurons):
-    """ Create the RNN model """
-    
-    # Reshape the data
-    train = train.reshape(train.shape[0], 1, train.shape[1])
-    
-    # Create the model
-    model = Sequential()
-    model.add(SimpleRNN(neurons, batch_input_shape=(batch_size, train.shape[1], train.shape[2]), stateful=True))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    
-    # Fit the model
-    for i in range(epochs):
-        model.fit(train, train, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
-        model.reset_states()
-    
-    # Return the model
-    return model
-
-
-
-# Another approach for handling missing values: Bayesian Structural Time Series = Bayesian structural time series is 
-# a Bayesian approach that models the underlying structure of a time series and the uncertainty surrounding it. The 
-# approach uses a hierarchical model to estimate the parameters of the model and the posterior distribution of the 
-# missing data. The posterior distribution can be used to generate imputations for the missing data, and the final 
-# estimates are obtained by averaging across the imputations. Bayesian structural time series is effective in 
-# handling missing data because it models the underlying structure of the time series and accounts for the 
-# uncertainty in the data.
-
-# Build a Bayesian structural time series model for impution of missing values, like so:
-from bsts import BVAR, BVARMAX
-from bsts.models import *
-
-# Implement the Bayesian structural time series model to impute missing values
-def create_bsts_model(train, test, p, q):
-    """ Create the Bayesian structural time series model """
-    
-    # Create the model
-    model = BVARMAX(train, p=p, q=q)
-    model.fit()
-    
-    # Make predictions
-    predictions = model.predict(steps=len(test))
-    
-    # Compute the error
-    error = mean_squared_error(test, predictions)
-    
-    # Return the predictions and the error
-    return predictions, error
-
-
-# State-Space Models: State-space models are a class of models that can capture the underlying dynamics of a time 
-# series, including trends, seasonal patterns, and other patterns that may be present in the data. These models can 
-# be used to impute missing values by estimating the unobserved states of the system that generated the time series. 
-# This approach can be especially useful for handling prolonged periods of missing data, as it can exploit the 
-# information contained in the observed data to make more accurate predictions.
-
-# Build a state-space model for impution of missing values, like so:
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-
-# Create the state-space model
-def create_state_space_model(train, test, p, d, q):
-    """ Create the state-space model """
-    
-    # Create the model
-    model = SARIMAX(train, order=(p, d, q))
-    model_fit = model.fit(disp=0)
-    
-    # Make predictions
-    predictions = model_fit.forecast(steps=len(test))
-    
-    # Compute the error
-    error = mean_squared_error(test, predictions)
-    
-    # Return the predictions and the error
-    return predictions, error
-
-
-
-
+    new, m_df = run_isolation_forest(new, i)
 
     
+c_df, m_df = run_isolation_forest(new, 'circumplex.arousal')[0]['anomaly'].sum()
+
+# Only select rows that contain 0 for the 'anomaly' column
+joe = joe[joe['anomaly'] == 0]
+
+# Impute missing values 
+from sklearn.impute import KNNImputer
+
+# KNN Imputation
+def impute_KNN(model_data: pd.DataFrame,
+               column_name: str,
+               n_neighbors = 10,
+               weights = 'uniform',
+               metric = 'nan_euclidean') -> pd.DataFrame:
+    # Impute missing values using KNN
+    imputer = KNNImputer(n_neighbors = n_neighbors,
+                         weights = weights,
+                         metric = metric)
+    df_copy = model_data.copy()
+    df_filled = imputer.fit_transform(df_copy[[column_name]])
+    
+    # Convert the array to a dataframe with a column name 'value'
+    df_filled = pd.DataFrame(df_filled, columns = ['value'])
+    
+    # Replace the 'value' column within df with the 'value' column from df_filled
+    df_copy['value'] = df_filled['value']
+
+    return df_copy
+
+# Impute missing values of the column 'value' with KNN
+knn = impute_KNN(data, 'value')
 
 
 
+
+
+
+
+
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import datetime
+
+np.array(data['variable'])
+
+def preprocess_data(input_data):
+    # Label encode the 'variable' column
+    encoder = LabelEncoder()
+    encoded_var = encoder.fit_transform(np.array(data['variable']))
+    
+    # Convert the array to a dataframe with a column name 'value'
+    df_enc = pd.DataFrame(encoded_var, columns = ['variable'])
+    
+    # Replace the 'value' column within df with the 'value' column from df_filled
+    input_data['variable'] = df_enc['variable']
+
+
+
+    encoded_var.columns = encoder.get_feature_names(['variable'])
+    input_data = pd.concat([input_data, encoded_var], axis=1)
+    input_data = input_data.drop('variable', axis=1)
+    
+    # Convert the 'time' column to Unix timestamps
+    input_data['time'] = pd.to_datetime(input_data['time'])
+    input_data['time'] = (input_data['time'] - datetime.datetime(1970,1,1)).dt.total_seconds()
+    
+    return input_data
+
+preprocess_data(data)
+
+
+
+
+
+#### BAYESIAN IMPUTATION
+import pytensor.tensor as pt
+
+with pm.Model() as model:
+    # Priors
+    mus = pm.Normal("mus", 0, 1, size=3)
+    cov_flat_prior, _, _ = pm.LKJCholeskyCov("cov", n=3, eta=1.0, sd_dist=pm.Exponential.dist(1))
+    # Create a vector of flat variables for the unobserved components of the MvNormal
+    x_unobs = pm.Uniform("x_unobs", 0, 100, shape=(np.isnan(data.values).sum(),))
+
+    # Create the symbolic value of x, combining observed data and unobserved variables
+    x = pt.as_tensor(data.values)
+    x = pm.Deterministic("x", pt.set_subtensor(x[np.isnan(data.values)], x_unobs))
+
+    # Add a Potential with the logp of the variable conditioned on `x`
+    pm.Potential("x_logp", pm.logp(rv=pm.MvNormal.dist(mus, chol=cov_flat_prior), value=x))
+    idata = pm.sample_prior_predictive()
+    idata = pm.sample()
+    idata.extend(pm.sample(random_seed=120))
+    pm.sample_posterior_predictive(idata, extend_inferencedata=True)
+
+pm.model_to_graphviz(model)
 
 
 
